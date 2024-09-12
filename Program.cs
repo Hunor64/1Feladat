25192ca8949b0e8cbf82c3d519693b12dd59ee62
hunor64 <nhunor06@gmail.com>
2024-09-12 12:42:11 +0200

    2. Feladat kész

--- Program.cs
+++ Program.cs
@@ -1,31 +1,66 @@
-﻿List<string> sorok = new();
+﻿//1. Feladat
+List<string> sorok = new();
 sorok = File.ReadAllLines("input.txt").ToList();
 int joSorokSzama = 0;
 foreach (var sor in sorok)
 {
-    int zarokSzama = 0;
-    if (sor[0] == ')')
+    if (sor.isValid())
     {
-
-    }
-    else
-    {
-        foreach (var betu in sor)
-        {
-            if (betu == '(')
-            {
-                zarokSzama++;
-            }
-            else if (betu == ')')
-            {
-                zarokSzama--;
-            }
-        }
-        if (zarokSzama == 0)
-        {
-            joSorokSzama++;
-        }
+        joSorokSzama++;
     }
 }
 Console.WriteLine($"1. Feladat: {joSorokSzama}");
 
+//2. Feladat
+Console.WriteLine("2. Feladat");
+Console.WriteLine("(())".isValid());
+Console.WriteLine("))((".isValid());
+
+//3. Feladat
+
+
+
+
+
+
+
+
+//2. Feladat
+static class StringExtensions
+{
+    public static bool isValid(this string sor)
+    {
+        int nyitokSzama = 0;
+        int zaroSzama = 0;
+        if (sor[0] == ')')
+        {
+            return false;
+        }
+        else
+        {
+            foreach (var betu in sor)
+            {
+                if (betu == '(')
+                {
+                    nyitokSzama++;
+                }
+                else
+                {
+                    zaroSzama++;
+                }
+                if (zaroSzama > nyitokSzama)
+                {
+                    return false;
+                }
+            }
+            if (nyitokSzama - zaroSzama == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
