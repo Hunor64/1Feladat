5b7efcdeb1686d5a28499e96d60fa934a6bf8cb7
hunor64 <nhunor06@gmail.com>
2024-09-12 11:53:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿List<string> sorok = new();
+sorok = File.ReadAllLines("input.txt").ToList();
+int joSorokSzama = 0;
+foreach (var sor in sorok)
+{
+    int zarokSzama = 0;
+    if (sor[0] == ')')
+    {
+
+    }
+    else
+    {
+        foreach (var betu in sor)
+        {
+            if (betu == '(')
+            {
+                zarokSzama++;
+            }
+            else if (betu == ')')
+            {
+                zarokSzama--;
+            }
+        }
+        if (zarokSzama == 0)
+        {
+            joSorokSzama++;
+        }
+    }
+}
+Console.WriteLine($"1. Feladat: {joSorokSzama}");
+
